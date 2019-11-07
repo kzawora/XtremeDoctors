c43ddc27ef0869244f01b3b3cd0c3788279d3303
domin92 <intunek2@interia.pl>
2019-11-07 19:37:46 +0100

    Add receptionist class

---
+++ XtremeDoctors/Models/Receptionist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XtremeDoctors.Models
+{
+    public class Receptionist
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        Receptionist(string name, string surname, string spec)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+    }
+}
