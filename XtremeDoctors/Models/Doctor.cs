eee040ef719a49b34e33ee779b524bd8e0050c34
domin92 <intunek2@interia.pl>
2019-11-07 19:29:39 +0100

    Add Doctor class

---
+++ XtremeDoctors/Models/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XtremeDoctors.Models
+{
+    public class Doctor
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Specialization { get; set; }
+
+        Doctor(string name, string surname, string spec)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Specialization = spec;
+        }
+
+    }
+}
