d72cf0db059a998a69529f3c7d9d39d5af92155e
Maciej Dziuban <maciej.dziuban@intel.com>
2019-11-26 21:27:02 +0100

    Add appointment list, it doesn't work because of reasons

--- XtremeDoctors/Services/AppointmentService.cs
+++ XtremeDoctors/Services/AppointmentService.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using XtremeDoctors.Models;
+using XtremeDoctors.Data;
 
 namespace XtremeDoctors.Services
 {
     public class AppointmentService
     {
+        private ApplicationDbContext database;
+        public AppointmentService(ApplicationDbContext database)
+        {
+            this.database = database;
+        }
+
+        public List<Appointment> GetAppointmentsForPatient(int patientId)
+        {
+            Patient patient = database.Patients.Find(patientId);
+            return patient.Appointments;
+        }
     }
 }
