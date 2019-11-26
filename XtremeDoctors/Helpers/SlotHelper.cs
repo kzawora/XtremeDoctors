ec839e3541b2c88c716143cce7f556c73ed2e094
Maciej Dziuban <maciej.dziuban@intel.com>
2019-11-26 19:57:53 +0100

    Add SlotHelper

---
+++ XtremeDoctors/Helpers/SlotHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XtremeDoctors.Helpers
+{
+    public class SlotHelper
+    {
+        public static string SlotToHour(int slot) {
+            int hour = slot / 4;
+            int minute = slot % 4;
+            return string.Format("{0}:{1}", hour, minute);
+        }
+    }
+}
