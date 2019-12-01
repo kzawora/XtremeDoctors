e7185c6ed29fcfbbef676f68840c198cf8fdc7d4
Yas <bartosz12364@gmail.com>
2019-12-01 20:38:10 +0100

    user class

---
+++ XtremeDoctors/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace XtremeDoctors.Models
+{
+    public class User : IdentityUser
+    {
+        [ForeignKey("DoctorId")]
+        public Doctor Doctor { get; set; }
+
+        [ForeignKey("PatientId")]
+        public Patient Patient { get; set; }
+    }
+}
