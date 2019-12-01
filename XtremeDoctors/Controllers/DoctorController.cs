e43cf112083119b4a21288c3d81765b06eb94f38
MaciejDziuban <dziuban.maciej@gmail.com>
2019-12-01 22:35:13 +0100

    Add working hours displaying

--- XtremeDoctors/Controllers/DoctorController.cs
+++ XtremeDoctors/Controllers/DoctorController.cs
@@ -10,6 +10,12 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace XtremeDoctors.Controllers
 {
+    public class DoctorViewModel
+    {
+        public Doctor doctor;
+        public string[] workingHours;
+    }
+
     [Authorize]
     [Route("[controller]")]
     public class DoctorController : Controller
@@ -32,17 +38,19 @@ namespace XtremeDoctors.Controllers
             [FromQuery(Name = "date")] DateTime date)
         {
             Doctor doctor = doctorService.FindDoctor(id);
-
             if (doctor == null)
             {
                 Response.StatusCode = 404;
                 return View("NotFound");
             }
 
-            ViewBag.doctor = doctor;
+            DoctorViewModel viewModel = new DoctorViewModel {
+                doctor = doctor,
+                workingHours = doctorService.GetHoursStringForWholeWeek(doctor),
+            };
 
+            ViewBag.doctorView = viewModel;
             ViewBag.freeHours = doctorService.ComputeFreeSlots(doctor, date);
-
             ViewBag.date = date;
 
             return View();
