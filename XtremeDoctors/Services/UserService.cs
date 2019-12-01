0611e7ece3320d86507090b18fea4e6ea3d70493
domin92 <intunek2@interia.pl>
2019-12-02 00:49:30 +0100

    user.patient

--- XtremeDoctors/Services/UserService.cs
+++ XtremeDoctors/Services/UserService.cs
@@ -9,8 +9,8 @@ namespace XtremeDoctors.Services
     public class UserService
     {
 
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly UserManager<User> _userManager;
+        private IHttpContextAccessor _httpContextAccessor;
+        private UserManager<User> _userManager;
         private ApplicationDbContext database;
         public UserService(ApplicationDbContext database, UserManager<User> userManager, IHttpContextAccessor _httpContextAccessor)
         {
@@ -30,12 +30,33 @@ namespace XtremeDoctors.Services
         {
             User user = await GetCurrentUser();
 
-            int patientId = 1;
-
-            if (user.Patient != null)
+            if(user == null)
             {
-                patientId = user.Patient.Id;
+                return 1; //Temp
             }
+
+            if (user.Patient == null)
+            {
+                Patient patient = new Patient();
+                patient.Name = "Jan";
+                patient.Surname = "Kowalski";
+
+
+                database.Patients.Add(patient);
+                database.SaveChanges();
+
+                user.Patient = patient;
+
+                var result = await _userManager.UpdateAsync(user); // This does not work kurwa jego mać!
+
+                if (!result.Succeeded)
+                {
+                    return 1; //Temp
+                }
+
+            }
+
+            int patientId = user.Patient.Id;
 
             return patientId;
         }
