3f7454ef49852fcb07bd750832c75296c7a1c93f
domin92 <intunek2@interia.pl>
2019-12-03 18:08:43 +0100

    Add name and surname in ragister page

--- XtremeDoctors/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ XtremeDoctors/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -59,6 +59,14 @@ namespace XtremeDoctors.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+            public string Name { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+            public string Surname { get; set; }
         }
 
         public void OnGet(string returnUrl = null)
@@ -78,7 +86,7 @@ namespace XtremeDoctors.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                Patient newPatient = createNewPatient("Kowalski", "Jan");
+                Patient newPatient = createNewPatient(Input.Name, Input.Surname);
                 var user = new User { UserName = Input.Email, Email = Input.Email, PatientId = newPatient.Id};
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
